65433ebe3144db833e1c70a7de6eacd7b22f4692
TheNofis <matbey122@gmail.com>
2025-01-22 02:00:07 +0300

    ref: edit mongoose connect

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import * as dotenv from "dotenv";
 import { Telegraf } from "telegraf";
-import mongoose from "mongoose";
 
 import InitStartCommand from "./command/init/Init.StartCommand.js";
 import InitTextCommand from "./command/init/Init.TextCommands.js";
 // import CallbackCommandInit from "./CallbackCommandInit.js";
 
-import mongoConnect from "./db/connect/mongodb.js";
+import "./db/connect/mongodb.js";
 dotenv.config();
 
 const token = process.env.BOT_TOKEN;
-const mongodbUrl = process.env.MONGODB_URI;
+
 class Bot {
   constructor(props) {
     this.bot = new Telegraf(props.BOT_TOKEN);
     this.initCommandList = [];
-    this.mongoDBUrl = mongodbUrl;
   }
   init() {
     const payload = this.bot;
@@ -27,7 +25,6 @@ class Bot {
 
     this.initCommandList.forEach((command) => command.handle());
 
-    mongoConnect();
     this.bot
       .launch()
       .then(() => console.log("Bot is ready"))
@@ -35,5 +32,5 @@ class Bot {
   }
 }
 
-const bot = new Bot({ BOT_TOKEN: token, MONGODB_URL: mongodbUrl });
+const bot = new Bot({ BOT_TOKEN: token });
 bot.init();
