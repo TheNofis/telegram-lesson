bfdc04e8bba55c8fe65bf9528ad93c9ca96b800c
TheNofis <matbey122@gmail.com>
2025-01-24 01:24:43 +0300

    ref: use findOneAndUpdate

--- src/command/text/Command.AcceptInvite.js
+++ src/command/text/Command.AcceptInvite.js
@@ -3,37 +3,40 @@ import Invite from "../../db/model/Invite.js";
 import CommandClass from "./Command.Class.js";
 
 import MenuMain from "../../menu/Menu.Main.js";
+import User from "../../db/model/User.js";
 
 export default class CommandSelect extends CommandClass {
-  constructor(props) {
-    super(props);
-  }
-
   async handle() {
-    const text = this?.ctx?.update?.message?.text;
-    const chatId = this?.ctx?.update?.message?.chat?.id;
-
-    if (!text.startsWith("/accept")) return;
+    if (!this.text.startsWith("/accept")) return;
 
     const uuid = text.split(" ")[1];
     const findInvite = await Invite.findOne({ invite: uuid });
 
-    if (findInvite == null || !findInvite.active || findInvite.count < 1) {
-      this.ctx.telegram.sendMessage(
+    if (findInvite == null || !findInvite.active || findInvite.count < 1)
+      return this.ctx.telegram.sendMessage(
         chatId,
         "Такого приглашения не найдено или оно не действительно",
         MenuMain,
       );
-      return;
-    }
 
-    findInvite.count--;
-    if (findInvite.count < 1) findInvite.active = false;
-    findInvite.save().catch((err) => console.error(err));
+    // Update invite status
+    Invite.findOneAndUpdate(
+      { invite: uuid },
+      {
+        $set: {
+          count: count - 1 > -1 ? count - 1 : 0,
+          active: count - 1 > -1 ? true : false,
+        },
+      },
+    ).catch((err) => console.error(err));
 
-    this.user.role = "admin";
-    this.user
-      .save()
+    // Update User
+    User.findOneAndUpdate(
+      { telegramId: this.user.telegramId },
+      {
+        $set: { admin: true },
+      },
+    )
       .then(() => {
         this.ctx.telegram
           .sendMessage(chatId, "✅ Вы приняли приглашение", MenuMain)
