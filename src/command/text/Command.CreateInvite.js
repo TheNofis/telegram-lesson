88341264e02211fec63c31a3c6334c8cdf72e5f5
TheNofis <matbey122@gmail.com>
2025-01-24 01:24:57 +0300

    ref: remoce constructor

--- src/command/text/Command.CreateInvite.js
+++ src/command/text/Command.CreateInvite.js
@@ -5,16 +5,9 @@ import CommandClass from "./Command.Class.js";
 import MenuMain from "../../menu/Menu.Main.js";
 
 export default class CommandSelect extends CommandClass {
-  constructor(props) {
-    super(props);
-  }
-
   async handle() {
-    const text = this?.ctx?.update?.message?.text;
-    const chatId = this?.ctx?.update?.message?.chat?.id;
-
     if (this.user.role != "admin") return;
-    if (!text.startsWith("/invite")) return;
+    if (!this.text.startsWith("/invite")) return;
 
     const count = Number(text.split(" ")[1]) || 1; // default count = 1
 
