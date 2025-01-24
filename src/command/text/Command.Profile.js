11f9b2a91ad3c9cf09663a4bc2c83c211f4c80c7
Teforch <matveev.gleb.boy@gmail.com>
2025-01-25 00:54:05 +0300

    fix: profile groupName

--- src/command/text/Command.Profile.js
+++ src/command/text/Command.Profile.js
@@ -6,10 +6,13 @@ export default class CommandSelect extends CommandClass {
   async handle() {
     if (this.text != "👤 Профиль") return;
 
+    const groupname =
+      this.user?.mgok?.groupName || this.user?.hexlet?.groupName;
+
     this.ctx.telegram
       .sendMessage(
         this.chatId,
-        `👤 Профиль\n\nℹ️ Имя: ${this.user.username}\n📚 Группа: ${this.user.groupName}\n\n✉️ Связаться: @thenofis\n🔗 GitHub: https://github.com/TheNofis/telegram-lesson`,
+        `👤 Профиль\n\nℹ️ Имя: ${this.user.username}\n📚 Группа: ${groupname}\n\n✉️ Связаться: @thenofis\n🔗 GitHub: https://github.com/TheNofis/telegram-lesson`,
         MenuMain,
       )
       .catch((err) => console.error(err));
