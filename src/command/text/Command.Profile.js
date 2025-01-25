6763e36ee7ea5ba9733a6d6ee1a3211b0440fb50
TheNofis <matbey122@gmail.com>
2025-01-25 04:00:13 +0300

    add: more information

--- src/command/text/Command.Profile.js
+++ src/command/text/Command.Profile.js
@@ -7,12 +7,17 @@ export default class CommandSelect extends CommandClass {
     if (this.text != "👤 Профиль") return;
 
     const groupname =
-      this.user?.mgok?.groupName || this.user?.hexlet?.groupName;
+      this.user?.mgok?.groupName ||
+      this.user?.hexlet?.groupName ||
+      "Не выбрана";
+    const college = this.user?.hexlet?.groupId ? "Hexlet" : "МГОК";
+    const course =
+      college == "МГОК" ? `🎓 Курс: ${this.user?.mgok?.course}\n` : "";
 
     this.ctx.telegram
       .sendMessage(
         this.chatId,
-        `👤 Профиль\n\nℹ️ Имя: ${this.user.username}\n📚 Группа: ${groupname}\n\n✉️ Связаться: @thenofis\n🔗 GitHub: https://github.com/TheNofis/telegram-lesson`,
+        `👤 Профиль\n\nℹ️ Имя: ${this.user.username}\n🏫 Колледж: ${college}\n${course}📚 Группа: ${groupname}\n\n✉️ Связаться: @thenofis\n🔗 GitHub: https://github.com/TheNofis/telegram-lesson`,
         MenuMain,
       )
       .catch((err) => console.error(err));
